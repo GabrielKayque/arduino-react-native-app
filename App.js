0161862e5c191440fc1d88558dbeb9522d72a3d5
Gabriel PC-Home <guilherme-lamon@hotmail.com>
2022-10-31 15:41:41 -0300

    Home Page and Info Page, zero design

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Button, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const Stack = createNativeStackNavigator()
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const HomeScreen = ({ navigation }) => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Tela de Pareamento</Text>
+        <Button
+          title="Go to InfoScreen"
+          onPress={() => navigation.navigate('Info')}
+        />
+      </View>
+    )
+  }
+
+  const InfoScreen = () => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Tela de Informações</Text>
+      </View>
+    )
+  }
+
+  return (
+    <NavigationContainer>
+      <StatusBar style="auto" />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Info" component={InfoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer >
+  )
+}
